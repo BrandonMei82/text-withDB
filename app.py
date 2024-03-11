c088443ffe0cc16de3f96d4e1090951cf33e4b9a
Teoh TeikToe <ttt@mensa.org.sg>
2024-03-11 15:55:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@
 
 from flask import Flask,request,render_template
 from textblob import TextBlob
+import sqlite3
+import datetime
 
 app = Flask(__name__)
 
@@ -18,6 +20,31 @@ def result():
     t = request.form.get("t")
     result = TextBlob(t).sentiment
     return(render_template("result.html",result=result))
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    name = request.form.get("name")
+    currentDateTime = datetime.datetime.now()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO user (name,timestamp) VALUES(?,?)',(name,currentDateTime))
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("register.html"))
+
+@app.route("/query",methods=["GET","POST"])
+def query():
+    conn = sqlite3.connect('database.db')
+    c = conn.execute('''select *
+        from user''')
+    r=""
+    for row in c:
+        print(row)
+        r = r + str(row)
+    c.close()
+    conn.close()
+    return(render_template("query.html",r=r))
 
 @app.route("/next", methods=["GET","POST"])
 def next():
